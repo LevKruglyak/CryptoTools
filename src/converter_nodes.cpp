e1ef2f03623505b8cd62fec804fc99f7d07ce526
LevKruglyak <lev.kruglyak2014@gmail.com>
2023-07-24 10:45:01 +0200

    add signed check

--- src/converter_nodes.cpp
+++ src/converter_nodes.cpp
@@ -1,10 +1,12 @@
 #include "graph.h"
+#include "imgui.h"
 #include "integer.h"
 #include "nodes.h"
 
 class IntegerToBufferNode : public Node {
   int in;
   int out;
+  bool sign = false;
 
 public:
   IntegerToBufferNode() : Node("Integer To Buffer") {
@@ -12,7 +14,7 @@ public:
     out = AddOutput("out", LinkType::BUFFER);
   }
 
-  bool DisplayInternal() override { return false; }
+  bool DisplayInternal() override { return ImGui::Checkbox("signed", &sign); }
 
   void ProcessInternal(Graph &graph) override {
     CryptoPP::Integer integer = GetInInteger(graph, in);
@@ -20,8 +22,14 @@ public:
     std::string str;
 
     str.resize(len);
-    integer.Encode((CryptoPP::byte *)str.data(), str.size(),
-                   CryptoPP::Integer::UNSIGNED);
+
+    if (sign) {
+      integer.Encode((CryptoPP::byte *)str.data(), str.size(),
+                     CryptoPP::Integer::SIGNED);
+    } else {
+      integer.Encode((CryptoPP::byte *)str.data(), str.size(),
+                     CryptoPP::Integer::UNSIGNED);
+    }
     SetOutLinkBuffer(graph, out, str);
   }
 };
@@ -29,6 +37,7 @@ public:
 class BufferToIntegerNode : public Node {
   int in;
   int out;
+  bool sign = false;
 
 public:
   BufferToIntegerNode() : Node("Buffer To Integer") {
@@ -36,12 +45,17 @@ public:
     out = AddOutput("out", LinkType::INTEGER);
   }
 
-  bool DisplayInternal() override { return false; }
+  bool DisplayInternal() override { return ImGui::Checkbox("signed", &sign); }
 
   void ProcessInternal(Graph &graph) override {
     std::string data = GetInBuffer(graph, in);
+    CryptoPP::Integer::Signedness sn = CryptoPP::Integer::UNSIGNED;
+    if (sign) {
+      sn = CryptoPP::Integer::SIGNED;
+    }
+
     CryptoPP::Integer integer =
-        CryptoPP::Integer((CryptoPP::byte *)data.data(), data.size());
+        CryptoPP::Integer((CryptoPP::byte *)data.data(), data.size(), sn);
     SetOutLinkInteger(graph, out, integer);
   }
 };
